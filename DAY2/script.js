40d0acfe53e001825530a91ff0b333e39f70ab6e
Satyajeeth Ophir Peruka <satyajeeth.peruka@gmail.com>
2026-07-16 23:11:46 +0530

    DAY2

--- DAY2/script.js
+++ DAY2/script.js
@@ -113,8 +113,6 @@
 
 // 1) MAP
 
-
-
 // const nums = [1, 2, 3, 4, 5, 6, 7, 8];
 // const result = nums.map((x) => x * 2);
 
@@ -162,7 +160,6 @@
 // console.log(calculate(5, double));
 // console.log(calculate(5, half));
 
-
 // conclusion:
 
 // Function Name
@@ -176,12 +173,194 @@
 
 // The result returned by the function
 
-
-
-// Callback Functions 
-
+// Callback Functions
 
 // Callback?
 // A function passed into another function that will be called LATER.
 
+// const hello = () => {
+//   console.log("HELLO");
+// };
+// // setInterval(hello,500);
 
+// //  Topic 6 - Returning Function
+
+// function multiplyBy(n) {
+//   return function (x) {
+//     return x * n;
+//   };
+// }
+
+// const double = multiplyBy(2);
+// const triple = multiplyBy(3);
+
+// console.log(double(3));
+// console.log(triple(3));
+
+// // Topic 7- Currying
+// function add(a) {
+//   return function (b) {
+//     return a + b;
+//   };
+// }
+
+// console.log("currying- " + add(2)(1));
+
+// // or with arrow functions
+
+// const aradd = (a) => (b) => a + b;
+
+// console.log("Arrow Currying- " + aradd(1)(2));
+
+// // EG Tax amount
+// const tax18 = (tax) => (price) => price + (price * tax) / 100;
+
+// const gst18 = tax18(18);
+
+// console.log(gst18(100));
+// console.log(gst18(500));
+
+// //  Topic -8 Function Composition
+
+// const getPrice = () => 1000;
+// const applyDiscount = (price) => price * 0.9;
+// const addGST = (price) => price * 1.18;
+// const generateInvoice = (total) => `₹${total}`;
+// const invoice = generateInvoice(addGST(applyDiscount(getPrice())));
+
+// console.log(invoice);
+
+// // Topic 9 - Website Flow
+
+// // Topic 10 - Callbacks -> Promise ->Async/Await
+
+// async function main(params) {
+//   const user = await getUser();
+//   const orders = await getOrders(user);
+//   const total = await calculatePrice(orders);
+
+//   display(total);
+// }
+
+// main();
+
+//  E-COMMERCE CHECKOUT
+//  --Need to place an Order
+
+function getUser() {
+  console.log("Fetching User");
+
+  return {
+    id: 101,
+    name: "SOP",
+  };
+}
+function getCart(user) {
+  console.log("Getting cart of " + user.name);
+
+  return [
+    { name: "Keyboard", price: 1000 },
+    { name: "Mouse", price: 500 },
+    { name: "Headphones", price: 2500 },
+  ];
+}
+function calculatePrice(cart) {
+  let total = 0;
+  for (const item of cart) {
+    total += item.price;
+  }
+  return total;
+}
+
+function applyDiscount(price) {
+  return price * 0.9; //10%
+}
+
+function addGST(price) {
+  return price * 1.18;
+}
+
+function genInvoice(total) {
+  return {
+    total,
+    invoiceNumber: "INV001",
+  };
+}
+function displayInvoice(invoice) {
+  console.log("Invoice:");
+
+  console.log(invoice);
+}
+
+const user = getUser();
+const cart = getCart(user);
+const total = calculatePrice(cart);
+const discountedPrice = applyDiscount(total);
+const finalPrice = addGST(discountedPrice);
+
+const invoice = genInvoice(finalPrice);
+
+displayInvoice(invoice);
+
+// Task - Student Result Management System
+
+function getStudent() {
+  return {
+    id: 1,
+    name: "SOP",
+  };
+}
+function getMarks(student) {
+  return {
+    math: 100,
+    chem: 100,
+    phy: 80,
+    biology: 100,
+    english: 100,
+  };
+}
+
+function calculateAverage(marks){
+    let total = 0;
+    for(const i in marks ){
+        total+=marks[i];
+    }
+    let avg = total/ Object.keys(marks).length;
+    return avg;
+}
+
+function assignGrade(avg){
+    if (avg >90 && avg <101){
+        return "A"
+    }
+    else if (avg >80 && avg <90){
+        return "B"
+    }
+    else if (avg >70 && avg <60){
+        return "C"
+        
+    }
+    else if (avg >40 && avg <50){
+        return "D"
+        
+    }
+    else{
+        return "F"
+    }
+}
+function genReportCard(grade,user,avg){
+    console.log("Report Card of "+user.name);
+    console.log(grade+" average: ",avg);
+}
+
+function displayReportCard(repocard){
+    console.log(repocard);
+}
+
+const student = getStudent();
+const marks  = getMarks(student);
+const average = calculateAverage(marks);
+const grade = assignGrade(average);
+const repotCard = genReportCard(grade,student,average);
+
+displayReportCard(repotCard)
